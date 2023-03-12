4d6291b710ab79bdafaa5835bdfb4cddd6418edb
pitagora <codxd13@gmail.com>
2023-03-12 23:32:30 +0100

    fixed the layout, better size consistency betweent pages

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -5,12 +5,14 @@ import glow from "../assets/hero-img.png"
 export default function About() {
     return (
         <div className="comp-wrapper">
-            <div>
-            <div className="the-gaming-world-hero">
-                <h1 className="hero-the">The</h1>
-                <h1 className="hero-gaming">Gaming</h1>
-                <h1 className="hero-world">World</h1>
-                    </div>
+            <div className="s">
+                        <div className="the-gaming-world-hero">
+                            <h1 className="hero-the">The</h1>
+                            <h1 className="hero-gaming">Gaming</h1>
+                            <h1 className="hero-world">World</h1>
+                        </div>
+                <img src={glow} alt="" className="fuck-css-and-my-life" />
+                </div>
                         <p>
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. 
                             Laudantium accusamus ipsum veniam hic alias velit deleniti ipsam consequatur molestiae quasi?
@@ -18,8 +20,8 @@ export default function About() {
                             aliquid corporis rem odit eius at dolores mollitia expedita maiores
                             explicabo omnis, natus ex hic debitis facilis ea.
                         </p>
-                    </div>
-            <img src={glow} className="hero-img"></img>
+            
+            {/*  <img src={glow} className="hero-img"></img> */}
         </div>
     )
 }
